ad90f14280665aa8fbfc4f850abbec2588b10855
Hamza <hamzabhai1297@gmail.com>
2023-05-31 15:26:57 +0500

    new form

--- login.php
+++ login.php
@@ -15,20 +15,17 @@
     <div class="form-heading text-center">
         <h2 class="mb-5">Login</h2>
     </div>
-  <form action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" name="remember"> Remember me</label>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
+    <form>
+        <div class="form-group">
+            <label for="user">Username</label>
+            <input type="text" id="user" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="user">Password</label>
+            <input type="password" id="password" class="form-control">
+        </div>
+        <a href="#" class="btn btn-primary">Submit</a>
+    </form>
 </div>
 
 </body>
